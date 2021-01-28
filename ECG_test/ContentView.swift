f20c417da3ec123f3beadbc4dc7998fffb86a426
rui <gongzeliuyi@gmail.com>
2021-01-28 11:17:00 +0900

    Initial Commit

---
+++ ECG_test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ECG_test
+//
+//  Created by 宮澤　留以 on 2021/01/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ECG_test/ECG_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECG_testApp.swift
+//  ECG_test
+//
+//  Created by 宮澤　留以 on 2021/01/28.
+//
+
+import SwiftUI
+
+@main
+struct ECG_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
